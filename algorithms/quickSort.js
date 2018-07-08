03ed423758b15cbf7817453ca684fa685f94dbe8
AhmadKabakibi <ahmadkbakibi@gmail.com>
2018-07-08 23:52:18 +0200

    sorting

---
+++ algorithms/quickSort.js
@@ -0,0 +1,40 @@
+const quicksort = (arr) => {
+    quicksortStep(arr, 0, arr.length - 1);
+};
+
+const quicksortStep = (arr, left, right) => {
+    if (left >= right) {
+        return;
+    }
+
+    const pivot = Math.floor((left + right) / 2);
+    const index = partition(arr, left, right, arr[pivot]);
+    quicksortStep(arr, left, index - 1);
+    quicksortStep(arr, index, right);
+};
+
+const partition = (arr, left, right, pivot) => {
+    while (left <= right) {
+        while (arr[left] < pivot) {
+            left = left + 1;
+        }
+
+        while (pivot < arr[right]) {
+            right = right - 1;
+        }
+
+        if (left <= right) {
+            swap(arr, left, right);
+            left = left + 1;
+            right = right - 1;
+        }
+    }
+
+    return left;
+};
+
+const swap = (arr, i, j) => {
+    const temp = arr[i];
+    arr[i] = arr[j];
+    arr[j] = temp;
+};
